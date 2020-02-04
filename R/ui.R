0f8e60810c8af12c3a224891adf11fc275f5bef8
nyujwc331 <60555205+nyujwc331@users.noreply.github.com>
2020-02-04 07:11:18 -0700

    Trying to add a simple module.

---
+++ R/ui.R
@@ -0,0 +1,24 @@
+#' Shiny app server object
+#'
+#' @import shiny
+
+# create the shiny application user interface
+ui <- fluidPage(
+
+pageWithSidebar(
+  headerPanel('Iris k-means clustering'),
+  sidebarPanel(
+    selectInput('xcol', 'X Variable', names(iris)),
+    selectInput('ycol', 'Y Variable', names(iris),
+                selected=names(iris)[[2]]),
+    numericInput('clusters', 'Cluster count', 3,
+                 min = 1, max = 9)
+  ),
+  mainPanel(
+    plotOutput('plot1'),
+
+    counterButton("counter1", "Counter #1")
+
+  )
+)
+)
---
+++ inst/shinyApp/app.R
@@ -0,0 +1,1 @@
+shinypackage::launchApp()
